0d19a5ea30562ddc3c1c324c0f212c5da07ce09f
ctaylor <hi.ctaylor.co@gmail.com>
2024-06-03 16:53:03 -0500

    Added JS keydown event

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+
+
+//When user presses the "1", "2" and "3" keys, the text changes.
+
+document.addEventListener("keydown", function (e) {
+    var key = e.key;
+    if (key === "1") {
+        box1.innerText = "🥚";
+    } else if (key === "2") {
+        box2.innerText = "🐣";
+    } else if (key === "3") {
+        box3.innerText = "🐥";
+    }
+    });
+
